951c7533b3dde2bb7626f2c5bf08587709e5cf2f
Rong Wu <43176832+Rong-Wu@users.noreply.github.com>
2021-05-07 21:40:30 -0500

    Add files via upload
    update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
 
-from flask import Flask, render_template, request, json, redirect
+from flask import Flask, render_template, request, json, redirect,url_for
 from flaskext.mysql import MySQL
 from flask import session
-from password_strength import PasswordPolicy
-from password_strength import PasswordStats
-
 from flask import jsonify
+import math
 
 app = Flask(__name__)
-
-policy = PasswordPolicy.from_names(
-    length=8,
-    uppercase=1,
-    numbers=1,
-    strength=0.66
-    )
 
 mysql = MySQL()
 
@@ -57,9 +48,11 @@ def showSignUp():
 
 @app.route('/signUp',methods=['POST']) 
 def signUp():
+    
     _name = request.form['inputName']
     _email = request.form['inputEmail']
     _password = request.form['inputPassword']
+    _isadmin = 0
  
     # validate the received values
     if _name and _email and _password:
@@ -75,17 +68,13 @@ def signUp():
         cursor.execute("SELECT * FROM tbl_user WHERE email = %s", (_email))
         if cursor.fetchone() is not None:   
             return render_template('signup.html', error = "That email is already taken, please choose another")
-        
-        stats = PasswordStats(password)
-        checkpolicy = policy.test(password)
-        if stats.strength() < 0.66:
-            return render_template('signup.html', error = "Password not strong ennough, Avoid consecutive characters and easily guessed words.")
-        
-        cursor.execute("INSERT INTO tbl_user(name, email, password) VALUES (%s, %s, %s)", (_name, _email, _password))
+
+        # create new user
+        cursor.execute("INSERT INTO tbl_user(name, email, password,is_admin) VALUES (%s, %s, %s, %s)", (_name, _email, _password, _isadmin))
         data = cursor.fetchall() 
         
         if len(data) == 0:
-            conn.commit() 
+            conn.commit()
             return render_template('signup.html', message = 'User created successfully !')
         else:
             return json.dumps({'error':str(data[0])})
@@ -115,13 +104,13 @@ def validateLogin():
 
 
         if len(data) > 0:
-            if str(data[0][3]) == _password:
+            if str(data[0][2]) == _password:
                 session['user_id'] = data[0][0]
-                return redirect('/home') #login success
+                return redirect('/') #login success
             else:
-                return json.dumps({'error': 'Wrong Email address or Password.'})
+                return render_template('signin.html', error = 'Password incorrect, please try enter correct password')
         else:
-            return json.dumps({'error': 'Wrong Email address or Password.'})
+            return render_template('signin.html', error = 'Email incorrect, please enter correct email')
 
     except Exception as e:
         return render_template('error.html',error = str(e))
@@ -140,9 +129,9 @@ def search():
         
         conn = mysql.connect()
         cursor = conn.cursor()
-
-        cursor.execute("SELECT * FROM tbl_product where name like %s",(_keyword))
-                                                                                                               
+        #SearchContent = "SELECT * FROM tbl_product where name like %{}% ".formet(_keyword)
+        #cursor.execute(SearchContent)
+        cursor.execute("SELECT * FROM tbl_product where name like %s ",('%'+_keyword+'%'))                                                                                                       
         data = cursor.fetchall()
 
         if len(data)>= 0 :
@@ -154,14 +143,63 @@ def search():
     except Exception as e:
         return render_template('error.html',error = str(e))
 
+
+@app.route("/Category", methods=['POST', 'GET'])
+def Category():
+    try:
+        if session.get('user_id'):
+            _userid =session.get('user_id')
+        
+        if request.method == 'GET':
+            cate_id=request.args.get('_id')
+        
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        #SearchContent = "SELECT * FROM tbl_product where name like %{}% ".formet(_keyword)
+        #cursor.execute(SearchContent)
+        cursor.execute("SELECT * FROM tbl_product where category_id= %s",(cate_id))                                                                                                    
+        data = cursor.fetchall()
+
+        if len(data)>= 0 :
+            conn.commit()
+            return render_template('SearchResult.html',result=data)
+        else:
+            return render_template ('error.html', error='no search result')
+       
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
+@app.route("/productinfo", methods=['POST', 'GET'])
+def productinfo():
+    if session.get('user_id'):
+        _userid =session.get('user_id')
+    try:
+        if request.method == 'GET':
+            product_id=request.args.get('_id')
+            session['product_id']= product_id
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+        #SearchContent = "SELECT * FROM tbl_product where name like %{}% ".formet(_keyword)
+        #cursor.execute(SearchContent)
+            cursor.execute("SELECT * FROM tbl_product where id= %s ",(product_id))                                                                                                      
+            data = cursor.fetchall()
+
+        if len(data)>= 0 :
+            conn.commit()
+            return render_template('productinfo.html',result=data)
+        else:
+            return render_template ('error.html', error='no search result')
+       
+    except Exception as e:
+        return render_template('error.html',error = str(e))
+
+
 @app.route('/logout')
 def logout():
     session.pop('user_id',None) #destroy the session
     return redirect('/')
 
-# @app.route('/profile')
-# def profile():
-    
 
 
 if __name__ == "__main__":
