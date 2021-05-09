ad3f5ef3fa9e6ff646a7e2bb4c9d22e3a7ae8173
Rong Wu <43176832+Rong-Wu@users.noreply.github.com>
2021-05-09 18:21:03 -0500

    Update tbl_order.sql

--- database/tbl_order.sql
+++ database/tbl_order.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Generation Time: May 09, 2021 at 06:44 AM
+-- Generation Time: May 09, 2021 at 11:20 PM
 -- Server version: 5.7.32
 -- PHP Version: 7.4.12
 
@@ -23,7 +23,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `tbl_order` (
   `order_id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
-  `purchase_date` date NOT NULL,
+  `purchase_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `total_price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -32,10 +32,11 @@ CREATE TABLE `tbl_order` (
 --
 
 INSERT INTO `tbl_order` (`order_id`, `user_id`, `purchase_date`, `total_price`) VALUES
-(1, 2, '2021-05-01', 10),
-(2, 2, '2021-05-08', 20.36),
-(3, 3, '2021-05-01', 33.5),
-(4, 3, '2021-05-09', 55.05);
+(1, 6, '2021-05-01 05:00:00', 10),
+(2, 6, '2021-05-08 05:00:00', 20.36),
+(3, 3, '2021-05-01 05:00:00', 33.5),
+(4, 3, '2021-05-09 05:00:00', 55.05),
+(12, 9, '2021-05-09 23:10:19', 19);
 
 --
 -- Indexes for dumped tables
@@ -55,4 +56,4 @@ ALTER TABLE `tbl_order`
 -- AUTO_INCREMENT for table `tbl_order`
 --
 ALTER TABLE `tbl_order`
-  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `order_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
